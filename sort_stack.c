f70af493acd833255ecf2f2e00d8e935602a4bfa
Hugo <hugoabouda@gmail.com>
2024-08-01 05:27:46 +0200

    lot of stuff done gl future me

--- sort_stack.c
+++ sort_stack.c
@@ -6,7 +6,7 @@
 /*   By: habouda <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/31 20:18:30 by habouda           #+#    #+#             */
-/*   Updated: 2024/08/01 04:06:58 by habouda          ###   ########.fr       */
+/*   Updated: 2024/08/01 05:27:25 by habouda          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,4 +38,45 @@ void	sort_three_stack(t_double_list **head)
 	if ((*head)->value < (second->next)->value)
 		return (swap(*head, 'a'));
 	return (rotate(head, 'a'));
+}
+
+
+void	sort_stack(t_double_list **a, t_double_list **b)
+{
+	push(b, a, 'b');
+	push(b, a, 'b');
+}
+
+int		cheapest_number(t_double_list **a, t_double_list **b)
+{ 
+	t_double_list	**c;
+	t_double_list	*head;
+
+	head = *b;
+	while(head->next)
+	{
+		alloc_temp_list(b, c);
+		ft_lstadd_end(c, head->value);
+		
+		ft_free_list(c);
+		head = head->next;
+	}
+	
+}
+
+void	alloc_temp_list(t_double_list **b, t_double_list **c)
+{
+	t_double_list *head;
+
+	if ((*b)->value == NULL)
+		return ;
+	head = *b;
+	c = malloc(sizeof(t_double_list)* 3);
+	if (!c)
+		return ;
+	while ((head->next) != NULL)
+	{
+		ft_lstadd_end(c, head->value);
+		head = head->next;
+	}
 }
